0122a60881134e8c057e1f58daa0c5c9f7e17d20
Stefan Halén <stefan.halen@iis.se>
2022-05-16 08:32:35 +0200

    Not needed

--- build-wheels.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-working_dir="/home/stefanh/docker/proxy-idp"
-cmd="/usr/local/bin/docker-compose"
-start_dir=`pwd`
-
-cd ${working_dir}
-${cmd} -f docker-compose-build.yaml run --rm py-builder 
-cd ${start_dir}
